ad6ea5dbe8b706456955226d409ba0fdbf5ddcc9
thdupin2 <theo.dupin03300@gmail.com>
2024-06-30 11:46:52 +0200

    :passport_control: add: guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { ListeRecettesComponent } from './components/liste-recettes/liste-recett
 import { LoginComponent } from './components/login/login.component';
 import {RecipeDetailComponent} from "./components/recipe-detail/recipe-detail.component";
 import { ListeIngredientsComponent } from './components/liste-ingredients/liste-ingredients.component';
+import { AuthGuard } from './core/models/guards/auth.guard';
 
 export const routes: Routes = [
     {
@@ -29,6 +30,7 @@ export const routes: Routes = [
     },
     {
         path: 'liste-ingredients',
-        component: ListeIngredientsComponent
+        component: ListeIngredientsComponent,
+        canActivate: [AuthGuard]
     }
 ];
